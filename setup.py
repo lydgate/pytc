fb6689573db4ad5c7e905d7d9b2343dace1fd06e
lydgate <git@vo.racio.us>
2010-10-19 13:38:31 +0100

    Initial setup.py for distutils.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+        name = 'pytc',
+        version = '0.2.2',
+        py_modules = ['pytc'],
+        description = 'Command line Twitter client',
+        author = 'Bryan Kam',
+        author_email = 'pytc@vo.racio.us',
+        url = 'http://twitter.com/pythontc',
+        keywords = ['twitter'],
+        classifiers = [
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.6'
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: End Users/Desktop',
+            'License :: OSI Approved :: GNU General Public License (GPL)',
+            'Natural Language :: English',
+            'Operating System :: POSIX :: Linux',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
+            ]
+)
