d1eefee903bb87df87c73896f737631cf79e84b5
Bryan Kam <gitorious@vo.racio.us>
2011-07-01 14:15:21 +0100

    Fixed some wrong version numbers, and small bugs in bit.ly updating.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
         name = 'pytc',
-        version = '0.3',
+        version = '0.3.1',
         py_modules = ['pytc'],
         description = 'Command line Twitter client',
         author = 'Bryan Kam',
